8ed5b98b47e6e14f638c97f1eb0c358725a1b82a
abeconnelly <abram@curoverse.com>
2014-11-24 18:40:35 -0500

    initial commit

---
+++ autoio.go
@@ -0,0 +1,233 @@
+package autoio
+
+import "fmt"
+import "os"
+import "io"
+import "encoding/json"
+import "bufio"
+
+import "errors"
+
+import "compress/gzip"
+import "compress/bzip2"
+
+import "flag"
+
+
+type AutoioContext struct {
+  Env map[string]string
+}
+
+func init() {
+  //fmt.Printf("bioenv init\n")
+}
+
+func (benv *AutoioContext) DebugPrint() {
+  for k,v := range benv.Env {
+    fmt.Println(k,v)
+  }
+}
+
+// Use the default config file to populate a variable map for common
+// file locations.
+//
+//func Autoio() (m map[string]string, err error)  {
+func Autoio() ( benv AutoioContext, err error ) {
+
+
+  fn := []string{ "/etc/bioenv", "/etc/bioenv/bioenv", "./.bioenv" }
+
+  if home := os.Getenv( "HOME" ) ; len(home) > 0 {
+    fn = append(fn, fmt.Sprintf("%s/%s", home, ".bioenv") )
+  }
+
+  for i := len(fn)-1; i>=0; i-- {
+
+    if _,e := os.Stat(fn[i]) ; os.IsNotExist(e) {
+      continue
+    }
+
+    file,e := os.Open( fn[i] )
+    if e != nil { continue }
+    decoder := json.NewDecoder(file)
+
+    //m = make( map[string]string )
+    benv.Env = make( map[string]string )
+
+    //if err := decoder.Decode(&m) ; err != nil {
+    if err = decoder.Decode(&(benv.Env)) ; err != nil {
+      return benv, err
+    }
+
+    //return m, nil
+    return benv, nil
+
+  }
+
+  //m = make( map[string]string )
+  benv.Env = make( map[string]string )
+
+
+  //return m, nil
+  return benv, nil
+
+}
+
+// Use specified config file to populate map
+//
+//func AutoioConfig( configFilename string ) (m map[string]string, err error)  {
+func AutoioConfig( configFilename string ) (benv AutoioContext, err error) {
+
+  if _,err := os.Stat( configFilename ) ; os.IsNotExist(err) {
+    return benv, err
+  }
+
+  file,err := os.Open( configFilename )
+  if err != nil { return benv, err }
+  decoder := json.NewDecoder(file)
+
+  //m = make( map[string]string )
+  benv.Env = make( map[string]string )
+  //if err := decoder.Decode(&m) ; err != nil {
+  if err := decoder.Decode(&(benv.Env)) ; err != nil {
+    return benv, err
+  }
+
+  //return m, nil
+  return benv, nil
+
+}
+
+// Default flags for bioenv
+func (benv *AutoioContext) ProcessFlag() {
+
+  flag.Visit( func( f *flag.Flag) { if f.Name == "reference" { benv.Env["refernece"] = f.Value.String() } } )
+}
+
+// Wrap common stream file types into one for ease of scanning
+//
+type AutoioHandle struct {
+  Fp *os.File
+  Scanner *bufio.Scanner
+  Writer *bufio.Writer
+
+  Bz2Reader io.Reader
+  GzReader *gzip.Reader
+  FileType string
+}
+
+// Magic strings we look for at the beginning of the file to determine file type.
+//
+var magicmap map[string]string = map[string]string{ "\x1f\x8b" : ".gz" , "\x1f\x9d" : ".Z", "\x42\x5a" : ".bz2" , "\x50\x4b\x03\x04" : ".zip" }
+
+func OpenScanner( fn string ) ( h AutoioHandle, err error ) {
+
+  if fn == "-" {
+    h.Fp = os.Stdin
+    h.Scanner = bufio.NewScanner( h.Fp )
+    return h, nil
+  }
+
+  var sentinalfp *os.File
+
+  sentinalfp,err = os.Open( fn )
+  if err != nil { return h, err }
+  defer sentinalfp.Close()
+
+
+  b := make( []byte, 2, 2 )
+  n,err := sentinalfp.Read(b)
+  if (n<2) || (err != nil) {
+    h.Fp,err = os.Open( fn )
+    if err != nil { return h, err }
+    h.Scanner = bufio.NewScanner( h.Fp )
+    return h, err
+  }
+
+  if typ,ok := magicmap[string(b)] ; ok {
+
+    h.Fp,err = os.Open( fn )
+    if err != nil { return h, err }
+
+    if typ == ".gz" {
+      h.FileType = "gz"
+
+      h.GzReader,err = gzip.NewReader( h.Fp )
+      if err != nil {
+        h.Fp.Close()
+        return h, err
+      }
+      h.Scanner = bufio.NewScanner( h.GzReader )
+    } else if typ == ".bz2" {
+
+      h.FileType = "bz2"
+
+      h.Bz2Reader = bzip2.NewReader( h.Fp )
+      h.Scanner = bufio.NewScanner( h.Bz2Reader )
+    } else {
+      err = errors.New(typ + "extension not supported")
+    }
+
+    return h, err
+  }
+
+
+  b2 := make( []byte, 2, 2)
+  n,err = sentinalfp.Read(b2)
+  if (n<2) || (err != nil) {
+    h.Fp,err = os.Open( fn )
+    if err != nil { return h, err }
+    h.Scanner = bufio.NewScanner( h.Fp )
+    return h, err
+  }
+
+  s := string(b) + string(b2)
+  if typ,ok := magicmap[s]; ok {
+    if typ == ".zip" {
+      err = errors.New("zip extension not supported")
+      return h, err
+    }
+    err = errors.New(typ + "extension not supported")
+    return h, err
+  }
+
+  h.Fp,err = os.Open( fn )
+  if err != nil { return h, err }
+  h.Scanner = bufio.NewScanner( h.Fp )
+
+  return h, err
+
+}
+
+func CreateWriter( fn string ) ( h AutoioHandle, err error ) {
+
+  if fn == "-" {
+    h.Fp = os.Stdout
+  } else {
+    h.Fp,err = os.Create( fn )
+    if err != nil { return h, err }
+  }
+
+  h.Writer = bufio.NewWriter( h.Fp )
+  return h, nil
+}
+
+func (h *AutoioHandle) Flush() {
+
+  if h.Writer != nil {
+    h.Writer.Flush()
+  }
+
+}
+
+func ( h *AutoioHandle) Close() error {
+
+  if h.FileType == "gz" {
+    e := h.GzReader.Close()
+    if e!=nil { return e }
+  }
+
+  e := h.Fp.Close()
+  return e
+
+}
---
+++ autoio_test.go
@@ -0,0 +1,89 @@
+package autoio
+
+import "fmt"
+import "testing"
+
+var g_verbose bool = false
+
+/*
+func TestFoo( t *testing.T ) {
+  fmt.Printf("testing...\n")
+}
+*/
+
+
+func TestConfigLoad( t *testing.T ) {
+  benv,err := Autoio()
+  if err!=nil { t.Errorf("could not load config file %s", err) }
+  _ = benv
+
+  //for k,v := range benv.Env { fmt.Println(k,v) }
+
+
+}
+
+
+func TestOpenTextFile( t *testing.T ) {
+  fn := "./magic/test.txt"
+
+  if g_verbose { fmt.Println(fn) }
+
+  h,err := OpenScanner( fn )
+  if err != nil { t.Errorf("got error %s", err ) }
+
+  for h.Scanner.Scan() {
+    l := h.Scanner.Text()
+
+    if g_verbose { fmt.Printf("  >>> %s\n", l) }
+
+    if l != "test" {
+      t.Errorf("did not read test from %s", fn )
+    }
+  }
+
+  h.Close()
+}
+
+func TestOpenGzFile( t *testing.T ) {
+  fn := "./magic/test.txt.gz"
+
+  if g_verbose { fmt.Println(fn) }
+
+  h,err := OpenScanner( fn )
+  if err != nil { t.Errorf("got error %s", err ) }
+
+  for h.Scanner.Scan() {
+    l := h.Scanner.Text()
+
+    if g_verbose { fmt.Printf("  >>> %s\n", l) }
+
+    if l != "test" {
+      t.Errorf("did not read test from %s", fn )
+    }
+  }
+
+  h.Close()
+}
+
+func TestOpenBzip2File( t *testing.T ) {
+  fn := "./magic/test.txt.bz2"
+
+  if g_verbose { fmt.Println(fn) }
+
+  h,err := OpenScanner( fn )
+  if err != nil { t.Errorf("got error %s", err ) }
+
+  for h.Scanner.Scan() {
+    l := h.Scanner.Text()
+
+    if g_verbose { fmt.Printf("  >>> %s\n", l) }
+
+    if l != "test" {
+      t.Errorf("did not read test from %s", fn )
+    }
+  }
+
+  h.Close()
+}
+
+
